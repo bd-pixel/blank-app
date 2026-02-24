4a3dbfb659cf9b358adced8d7575d36fc3aa998d
bd-pixel <79328645+bd-pixel@users.noreply.github.com>
2026-02-24 15:16:44 +0000

    First template

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,47 @@
 import streamlit as st
+import numpy as np
+import plotly.graph_objects as go
+import time
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Live Electricity Price")
+
+if "running" not in st.session_state:
+    st.session_state.running = False
+
+if st.button("Start Simulation"):
+    st.session_state.running = True
+
+if st.session_state.running:
+
+    placeholder = st.empty()
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=[], y=[], mode="lines"))
+
+    fig.update_layout(
+        xaxis_title="Time",
+        yaxis_title="Price (ct/kWh)",
+        yaxis_range=[30, 90],
+    )
+
+    data_x = []
+    data_y = []
+
+    shock_minute = 30
+
+    for minute in range(60):
+
+        if minute < shock_minute:
+            price = 40 + np.random.normal(0, 0.2)
+        else:
+            price = 75 + np.random.normal(0, 0.5)
+
+        data_x.append(minute)
+        data_y.append(price)
+
+        fig.data[0].x = data_x
+        fig.data[0].y = data_y
+
+        placeholder.plotly_chart(fig, use_container_width=True)
+
+        time.sleep(0.1)
