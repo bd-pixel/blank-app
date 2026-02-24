a49478552c5c1a7dd084ca616dc3790f5ee81f89
bd-pixel <79328645+bd-pixel@users.noreply.github.com>
2026-02-24 15:17:44 +0000

    Hello

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,15 +21,15 @@ if st.session_state.running:
     fig.update_layout(
         xaxis_title="Time",
         yaxis_title="Price (ct/kWh)",
-        yaxis_range=[30, 90],
+        yaxis_range=[0, 90],
     )
 
     data_x = []
     data_y = []
 
-    shock_minute = 30
+    shock_minute = 50
 
-    for minute in range(60):
+    for minute in range(90):
 
         if minute < shock_minute:
             price = 40 + np.random.normal(0, 0.2)
@@ -44,4 +44,4 @@ if st.session_state.running:
 
         placeholder.plotly_chart(fig, use_container_width=True)
 
-        time.sleep(0.1)
+        time.sleep(0.01)
